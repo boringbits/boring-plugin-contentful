4072b58e6cd0e4d7b8ff93a44f2ae02ebb0c46af
Ryan Stevens <stevens.ryan@gmail.com>
2019-04-03 12:33:01 -0700

    fix bump

--- src/test/contentfulAPI-test.js
+++ src/test/contentfulAPI-test.js
@@ -2,25 +2,27 @@ const assert = require('assert');
 const contentful = require('contentful');
 
 
-jest.mock('contentful', () => {
-  return {
-    createClient: (config) => {
-      const realClient = contentful.createClient(config);
-      return {
-        getEntries: config.getEntries,
-        parseEntries: realClient.parseEntries
-      };
-    }
-  }
-})
-
 function createApi(getEntriesFn) {
-  const contentful = require('../managed_modules/contentful');
+
+  jest.mock('contentful', () => {
+
+    return {
+      createClient: (config) => {
+
+        const realClient = contentful.createClient(config);
+        return {
+          getEntries: getEntriesFn,
+          parseEntries: realClient.parseEntries
+        };
+      }
+    }
+  })
+
+  const contentfulApi = require('../managed_modules/contentful');
 
   let Api;
 
   const configObj = {
-    'clients.contentful': { 'accessToken': 'bar', 'space': 'bee', getEntries: 'bar' },
     'clients.contentful.accessToken': 'bar',
     'clients.contentful.space': 'bar',
     // this is kinda a hacky to pass this function into the
@@ -40,7 +42,7 @@ function createApi(getEntriesFn) {
     config: { get: (key) => configObj[key] }
   }
 
-  contentful(injections);
+  contentfulApi(injections);
   return new Api();
 }
 
