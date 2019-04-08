3f4835d398ff6d5e0b717dc35625302541b78d6b
Ryan Stevens <stevens.ryan@gmail.com>
2019-04-08 16:30:34 -0700

    only render position arrounds when needed

--- src/client/pages/sitemap/components/SiteTree.js
+++ src/client/pages/sitemap/components/SiteTree.js
@@ -12,6 +12,7 @@ function NodeLabel(props) {
   const [hovering, setHover] = useState(false);
 
   const style = {
+    marginLeft: (expanderSize /2) +'px',
     width: (props.nodeWidth * 2) + 'px',
     height: (props.nodeHeight + textPaddingTop) +'px',
     //backgroundColor: 'red',
@@ -21,9 +22,27 @@ function NodeLabel(props) {
     width: expanderSize+'px',
     height: expanderSize+'px',
     lineHeight: ((expanderSize/2) + 5 ) + 'px',
-    left: ((props.nodeWidth - expanderSize) / 2) + 'px',
+    left:  (expanderSize /2)+ ((props.nodeWidth - expanderSize) / 2) + 'px',
     backgroundColor: '#5c6bc0',
   };
+
+  const moveLeftStyle = {
+    width: expanderSize+'px',
+    height: expanderSize+'px',
+    lineHeight: ((expanderSize/2) + 5 ) + 'px',
+    bottom: (props.nodeHeight/2) + 'px',
+    left: '0px',
+    backgroundColor: '#5c6bc0',
+  }
+
+  const moveRightStyle = {
+    width: expanderSize+'px',
+    height: expanderSize+'px',
+    lineHeight: ((expanderSize/2) + 5 ) + 'px',
+    bottom: (props.nodeHeight/2) + 'px',
+    left:  (expanderSize /2)+ (props.nodeWidth - (expanderSize/2))  + 'px',
+    backgroundColor: '#5c6bc0',
+  }
 
   const {nodeData} = props;
   const space = window.app_vars.config.contentful.space;
@@ -66,24 +85,34 @@ function NodeLabel(props) {
     <div style={style} onClick={maskClick} className={'nodeMask ' + ((hovering) ? 'hovering' : 'nothovering')} onMouseOver={over} onMouseOut={out}>
       <h6>{nodeData.name}</h6>
       <a href={manageLink} onClick={manageClick}>manage</a>
-      { (pageUrl) ? <a href={pageUrl} onClick={pageClick}>page</a>: <></> }
-      { (nodeData._children && nodeData._children.length>0) ?
-        <div style={expandStyle} className={'expand'} onClick={expandClick}>
-          {nodeData._collapsed ? '+' : '-'}
-        </div>
-        : <></>}
+        { (pageUrl) ? <a href={pageUrl} onClick={pageClick}>page</a>: <></> }
+        { (nodeData._children && nodeData._children.length>0) ?
+          <div style={expandStyle} className={'charButton'} onClick={expandClick}>
+            {nodeData._collapsed ? '+' : '-'}
+          </div>
+          : <></>
+        }
+
+        {nodeData.child_pos > 0 ? <div style={moveLeftStyle} className={'position charButton'}>&lt;</div> : <></>}
+        {nodeData.child_pos < (nodeData.children_length-1) ? <div style={moveRightStyle} className={'position charButton'}>&gt;</div> : <></>}
+
     </div>
   )
 
 }
 
 function mapContent(item) {
+  let childPos = 0;
   return {
     ...item.content,
     contentful_id: item.id,
     name: (item.content.name || item.content.title),
     children: ((item.content.children && item.content.children.length>0) ? item.content.children.map(child => {
-      return mapContent(child);
+      return {
+        ...mapContent(child),
+        child_pos: childPos++,
+        children_length: item.content.children.length
+      }
     }) : [])
   }
 }
@@ -173,8 +202,8 @@ class SiteTree extends React.Component {
               render: <NodeLabel nodeWidth={this.state.dims.node.width} nodeHeight={this.state.dims.node.height} />,
               foreignObjectWrapper: {
                 y: (((this.state.dims.node.height / 2) * -1) - textPaddingTop),
-                x: ((this.state.dims.node.width / 2) * -1),
-                width: (this.state.dims.node.width * 2),
+                x: ((this.state.dims.node.width / 2) * -1) - (expanderSize/2),
+                width: (this.state.dims.node.width * 2) + (expanderSize/2),
                 height: (this.state.dims.node.height + textPaddingTop + (expanderSize/2))
               }
             }}
